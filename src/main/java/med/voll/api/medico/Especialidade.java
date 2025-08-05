6b4582b8ed64dfb4a38454ad62f65ddf7007ef30
VitoSB UESB <vitorsantiagobarreto123@gmail.com>
2025-08-05 02:19:20 -0300

    JPA e Requisições de GET

--- src/main/java/med/voll/api/medico/Especialidade.java
+++ src/main/java/med/voll/api/medico/Especialidade.java
@@ -2,10 +2,23 @@ package med.voll.api.medico;
 
 public enum Especialidade {
 
-    ORTOPEDIA,
-    CARDIOLOGIA,
-    GINECOLOGIA,
-    DERMATOLOGIA;
+    ORTOPEDIA("ORTOPEDIA"),
+    CARDIOLOGIA("CARDIOLOGIA"),
+    GINECOLOGIA("GINECOLOGIA"),
+    DERMATOLOGIA("DERMATOLOGIA");
 
+    private String transformar;
 
+    Especialidade(String transformar){
+        this.transformar = transformar;
+    }
+
+    public static Especialidade fromString(String txt) {
+        for (Especialidade especialidade : Especialidade.values()) {
+            if (especialidade.transformar.equalsIgnoreCase(txt)) {
+                return especialidade;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + txt);
+    }
 }
